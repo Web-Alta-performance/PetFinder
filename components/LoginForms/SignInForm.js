90aaf8d5e62ccd1cf919250e2aefdc1e8249abdd
MarcioSobel <marcinhosobel@hotmail.com>
2024-03-29 11:22:45 -0300

    merge formStyle into userForm

--- components/LoginForms/SignInForm.js
+++ components/LoginForms/SignInForm.js
@@ -1,6 +1,5 @@
-import { Pressable, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { UserForm, FormUserInput, FormUserButton, FormUserLink } from '../UserForm';
-import formStyle from './formStyle';
 import { useState } from 'react';
 
 const SignInForm = ({ navigation }) => {
@@ -9,23 +8,21 @@ const SignInForm = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-
     return (
         <UserForm
-            style={formStyle.form}
             height={400}
             navigation={navigation}
         >
-            <FormUserInput style={formStyle} onChange={setEmail} value={email}>E-MAIL</FormUserInput>
-            <FormUserInput style={formStyle} onChange={setPassword} value={password} secure>SENHA</FormUserInput>
-            <FormUserButton style={formStyle} color={'#271810'}>ENTRAR</FormUserButton>
+            <FormUserInput onChange={setEmail} value={email}>E-MAIL</FormUserInput>
+            <FormUserInput onChange={setPassword} value={password} secure>SENHA</FormUserInput>
+            <FormUserButton color={'#271810'}>ENTRAR</FormUserButton>
             <View style={{
                 alignItems: 'center',
                 justifyContent: 'center',
                 gap: 10,
             }}>
-                <FormUserLink style={formStyle}>Esqueci a senha</FormUserLink>
-                <FormUserLink style={formStyle}>Crie uma conta aqui</FormUserLink>
+                <FormUserLink>Esqueci a senha</FormUserLink>
+                <FormUserLink onPress={() => navigation.navigate('SignUp')}>Crie uma conta aqui</FormUserLink>
             </View>
         </UserForm>
     )
--- components/UserForm.js
+++ components/UserForm.js
@@ -1,14 +1,15 @@
 import { Pressable, Text, TextInput, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import GlassView from './GlassView';
 
-export const FormUserInput = ({value, secure, onChange, children, style}, props) => {
+export const FormUserInput = ({ value, secure, onChange, children }) => {
     return (
         <View style={{ gap: 10, width: '100%', alignItems: 'center' }}>
-            <Text style={style.text}>
+            <Text style={[styles.text, Foreground]}>
                 {children}
             </Text>
             <TextInput
-                style={style.input}
+                style={[styles.input, Foreground]}
                 secureTextEntry={secure}
                 autoCapitalize='none'
                 autoCorrect={false}
@@ -19,38 +20,108 @@ export const FormUserInput = ({value, secure, onChange, children, style}, props)
     )
 }
 
-export const FormUserButton = ({onPress, style, children, color}, props) => {
+export const FormUserButton = ({ onPress, children, color }) => {
     const buttonBg = {backgroundColor: color}
     return (
         <Pressable onPress={() => onPress()}>
-            <View style={{...style.button, ...buttonBg}}>
-                <Text style={style.buttonText}>{children}</Text>
+            <View style={[{...styles.button, ...buttonBg}, Foreground]}>
+                <Text style={[styles.buttonText, Foreground]}>{children}</Text>
             </View>
         </Pressable>
     )
 }
 
-export const FormUserLink = ({onPress, style, children}, props) => {
+export const FormUserLink = ({ onPress, children }) => {
     return (
-        <Pressable onPress={onPress} style={style.link}>
-            <Text style={style.linkText}>{children}</Text>
+        <Pressable onPress={(onPress)} style={Foreground}>
+            <Text style={[styles.linkText, Foreground]}>{children}</Text>
         </Pressable>
     )
 }
 
-export const UserForm = ({ style, children, height }) => {
+export const UserForm = ({ children, height }) => {
+
+    // FIXME: blur currently breaks navigation
+    isBlurBroken = true;
 
     return (
         <GlassView
-            style={{justifyContent: 'center', alignItems: 'center', ...style, height}}
-            tint={'systemThickMaterialLight'}
+            style={{justifyContent: 'center', alignItems: 'center', ...styles.form, height, Foreground}}
             gradientProps={{
                 colors: ['rgba(255, 255, 255, 0.5)', 'rgba(255, 255, 255, 0.0)'],
                 start: { x: 0, y: 0 },
                 end: { x: 0.75, y: 1 }
             }}
+            intensity={isBlurBroken ? 0 : 12}
             >
             {children}
         </GlassView>
     );
 };
+
+const Foreground = { zIndex: 10 }
+
+const styles = StyleSheet.create({
+    form: {
+        width: '95%',
+        height: 350,
+        maxWidth: 450,
+        borderRadius: 15,
+        borderWidth: 2,
+        
+        justifyContent: 'center',
+        gap: 20,
+    },
+
+    // text
+    text: {
+        color: 'white',
+        fontFamily: 'Quicksand-Bold',
+        fontSize: 17,
+    },
+    buttonText: {
+        color: 'white',
+        fontFamily: 'Quicksand-Bold',
+        fontSize: 15,
+        fontSize: 23,
+    },
+    linkText: {
+        color: 'white',
+        fontFamily: 'Quicksand-Bold',
+        fontSize: 17,
+    }, 
+
+    // ui
+    input: {
+        fontFamily: 'Quicksand-Medium',
+        fontSize: 15,
+
+        backgroundColor: 'white',
+        width: '90%',
+        borderRadius: 10,
+        height: 40,
+        paddingHorizontal: 10,
+    },
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+        marginTop: 15,
+        
+        paddingHorizontal: 24,
+        paddingTop: 10,
+        paddingBottom: 12,
+        
+        borderRadius: 15,
+
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        
+        elevation: 8,
+    },
+});
