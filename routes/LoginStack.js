21f1d528bc0749311f8525b448e698910dd6e032
marciosobel <marcinhosobel@hotmail.com>
2024-04-13 18:56:09 -0300

    update imports

--- routes/LoginStack.js
+++ routes/LoginStack.js
@@ -2,19 +2,19 @@ import { View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // screens
-import SignIn from '../screens/SignIn';
-import SignUp from '../screens/SignUp';
-import ForgotPassword from '../screens/ForgotPassword';
+import SignIn from '../screens/Authentication/SignIn';
+import SignUp from '../screens/Authentication/SignUp';
+import ForgotPassword from '../screens/Authentication/ForgotPassword';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 const LoginStack = () => {
     return (
-        <View style={{flex: 1, width: '100%', height: '100%'}}>
-            <Navigator initialRouteName="SignIn" screenOptions={{headerShown: false}}>
-                <Screen name="SignIn" component={SignIn}/>
-                <Screen name="SignUp" component={SignUp}/>
-                <Screen name="ForgotPassword" component={ForgotPassword}/>
+        <View style={{ flex: 1, width: '100%', height: '100%' }}>
+            <Navigator initialRouteName="SignIn" screenOptions={{ headerShown: false }}>
+                <Screen name="SignIn" component={SignIn} />
+                <Screen name="SignUp" component={SignUp} />
+                <Screen name="ForgotPassword" component={ForgotPassword} />
             </Navigator>
         </View>
     );
