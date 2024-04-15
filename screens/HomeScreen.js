c15a209038e801d2b7d73d1cfa7a643524ad96cb
marciosobel <marcinhosobel@hotmail.com>
2024-04-14 22:29:52 -0300

    display random info

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,17 @@
 import { Text } from "react-native";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 const HomeScreen = () => {
-    return (
-        <Text>Salve</Text>
+
+    const { userInfo } = useContext(AuthContext);
+
+    return (<>
+        <Text>Your info:</Text>
+        <Text>Name: {userInfo.name}</Text>
+        <Text>Email: {userInfo.email}</Text>
+        <Text>Phone: {userInfo.phone_number || 'non-existent'}</Text>
+    </>
     );
 };
 
