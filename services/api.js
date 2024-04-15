1d9135694314a2e561a318b8c54ce9ca65f26a8e
marciosobel <marcinhosobel@hotmail.com>
2024-04-14 22:21:20 -0300

    add api requests

--- services/api.js
+++ services/api.js
@@ -1,7 +1,40 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'http://192.168.1.8:3000'
 });
+
+export const userRegister = async ({
+    name,
+    email,
+    password,
+}) => {
+    const response = await api.post('/users', {
+        name,
+        email,
+        password
+    });
+
+    return response.body;
+}
+
+export const userLogin = async ({
+    email,
+    password,
+}) => {
+    const response = await api.post('/users/auth', {
+        email,
+        password
+    });
+
+    return response.data;
+}
+
+export const userChangePassword = async ({
+    email,
+    newPassword,
+}) => {
+    alert('Essa função ainda está em progresso. Por favor, contate um administrador.');
+};
 
 export default api;
