532e7636021a178b9caf27caaa9ca2f460417aaa
marciosobel <marcinhosobel@hotmail.com>
2024-04-17 10:01:30 -0300

    create settings screen

---
+++ screens/Settings.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Settings = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Configuraçoes ebaa</Text>
+        </View>
+    )
+};
+
+export { Settings };
