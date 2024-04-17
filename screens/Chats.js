c3573ef451ed41b3d3b56b0e74aeea95672da385
marciosobel <marcinhosobel@hotmail.com>
2024-04-17 10:01:38 -0300

    create chats screen

---
+++ screens/Chats.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Chats = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Chats ebaa</Text>
+        </View>
+    )
+};
+
+export { Chats };
