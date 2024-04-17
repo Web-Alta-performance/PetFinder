324ccfec2839020fc85b1cce702b374ef318880f
marciosobel <marcinhosobel@hotmail.com>
2024-04-17 10:01:53 -0300

    create account screen

---
+++ screens/Account.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Account = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Account ebaa</Text>
+        </View>
+    )
+};
+
+export { Account };
