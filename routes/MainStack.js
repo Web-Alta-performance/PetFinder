c6e429793e0508617cd558f37bbc0425e7a2a87a
marciosobel <marcinhosobel@hotmail.com>
2024-04-17 10:02:04 -0300

    add screens to main stack

--- routes/MainStack.js
+++ routes/MainStack.js
@@ -1,23 +1,28 @@
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { CustomDrawer } from '../components/CustomDrawer';
 
-import { HomeScreen } from '../screens/HomeScreen';
-import { Image } from "react-native";
+// Screens
+import { Home } from '../screens/Home';
+import { Settings } from '../screens/Settings';
+import { Chats } from '../screens/Chats';
+import { Account } from '../screens/Account';
+import { Support } from '../screens/Support';
+import { About } from '../screens/About';
 
 const MainStack = () => {
 
     const Drawer = createDrawerNavigator();
 
     return (
-        <Drawer.Navigator screenOptions={{ title: '' }}>
-            <Drawer.Screen
-                component={HomeScreen}
-                name='Home'
-                options={{
-                    drawerIcon: ({ color, size }) => <Ionicons name='home' color={color} size={size} />,
-                    drawerLabel: 'Início',
-                }}
-            />
+        <Drawer.Navigator
+            screenOptions={{ headerShown: false }}
+            drawerContent={props => <CustomDrawer {...props} />}>
+            <Drawer.Screen name='Home' component={Home} />
+            <Drawer.Screen name='Settings' component={Settings} />
+            <Drawer.Screen name='Chats' component={Chats} />
+            <Drawer.Screen name='Account' component={Account} />
+            <Drawer.Screen name='Support' component={Support} />
+            <Drawer.Screen name='About' component={About} />
         </Drawer.Navigator>
     );
 };
