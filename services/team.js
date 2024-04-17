9feb65b6e9e12b2de69f53f41cb35d8e7a4fa252
marciosobel <marcinhosobel@hotmail.com>
2024-04-17 10:00:48 -0300

    move team to a separate file

---
+++ services/team.js
@@ -0,0 +1,54 @@
+export const team = [
+    {
+        name: 'Carlos Ramalho',
+        function: 'Líder',
+    },
+    {
+        name: 'Márcio Sobel',
+        function: 'Designer, dev full-stack',
+    },
+    {
+        name: 'Gustavo Portella',
+        function: 'Designer',
+    },
+    {
+        name: 'Rafael Antônio Mendes',
+        function: 'Tester',
+    },
+    {
+        name: 'José Gabriel de Oliveira Lino',
+        function: 'Tester',
+    },
+    {
+        name: 'Laryssa Rayanne Souza Martins',
+        function: 'Tester',
+    },
+    {
+        name: 'Rafael Ferreira dos Anjos',
+        function: 'Tester'
+    },
+    {
+        name: 'Débora Vitória',
+        function: 'Supporter'
+    },
+    {
+        name: 'Diego Rodrigues',
+        function: 'Supporter'
+    },
+    {
+        name: 'Eric Mendonça',
+        function: 'Supporter',
+    },
+    {
+        name: 'Jessica Nascimento',
+        function: 'Supporter',
+    },
+    {
+        name: 'João Guilherme Caetano',
+        function: 'Supporter',
+    },
+    {
+        name: 'João Victor Mendonça',
+        function: 'Supporter',
+    },
+]
