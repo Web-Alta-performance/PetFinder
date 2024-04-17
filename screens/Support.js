b83a6644938bcc89aef2c963aba2fdad22b85d0f
marciosobel <marcinhosobel@hotmail.com>
2024-04-17 10:01:24 -0300

    craete support screen

---
+++ screens/Support.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Support = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Support ebaa</Text>
+        </View>
+    )
+};
+
+export { Support };
